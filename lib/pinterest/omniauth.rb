3487b79973b638b70a90533b345007a8f5066981
Adam Jacox <yellowjacox@gmail.com>
2016-11-15 18:55:53 -0700

    Include additional user fields in raw_info

--- lib/pinterest/omniauth.rb
+++ lib/pinterest/omniauth.rb
@@ -4,15 +4,16 @@ module OmniAuth
   module Strategies
     class Pinterest < OmniAuth::Strategies::OAuth2
       option :client_options, {
-        :site => 'https://api.pinterest.com/',
-        :authorize_url => 'https://api.pinterest.com/oauth/',
-        :token_url => 'https://api.pinterest.com/v1/oauth/token'
+        site:          'https://api.pinterest.com/',
+        authorize_url: 'https://api.pinterest.com/oauth/',
+        token_url:     'https://api.pinterest.com/v1/oauth/token'
       }
+
+      option :fields, 'id,first_name,last_name,url,username'
 
       def request_phase
         options[:scope] ||= 'read_public,write_public'
         options[:response_type] ||= 'token'
-        #options[:state] ||= '22'
         super
       end
 
@@ -21,7 +22,11 @@ module OmniAuth
       info { raw_info }
 
       def raw_info
-        @raw_info ||= access_token.get('/v1/me/').parsed['data']
+        @raw_info ||= access_token.get('/v1/me/', params: { fields: options.fields }).parsed['data']
+      end
+
+      def ssl?
+        true
       end
     end
   end
